84f029abc77188a8c115ead78d6a5c7f39e1cb07
aminnairi <18418459+aminnairi@users.noreply.github.com>
2023-07-04 11:18:13 +0200

    added a new column for the confirmation of the account

--- sources/migration.php
+++ sources/migration.php
@@ -2,6 +2,10 @@
 
 namespace App;
 
+ini_set("display_errors", "1");
+ini_set("display_startup_errors", "1");
+error_reporting(E_ALL);
+
 require_once __DIR__ . "/autoload.php";
 
 use App\Repositories\Repository;
@@ -13,9 +17,25 @@ try {
     $databaseConnection = $repository->getDatabaseConnection();
 
     $databaseConnection->query("DROP TABLE IF EXISTS users;");
-    $databaseConnection->query("CREATE TABLE users(id INTEGER PRIMARY KEY AUTO_INCREMENT, email VARCHAR(50) UNIQUE NOT NULL, password CHAR(60) NOT NULL, token CHAR(60) NULL)");
+
+    $databaseConnection->query("
+            CREATE TABLE users(
+            id INTEGER PRIMARY KEY AUTO_INCREMENT,
+            email VARCHAR(50) UNIQUE NOT NULL,
+            password CHAR(60) NOT NULL,
+            authentication_token CHAR(60) NULL,
+            confirmation_token CHAR(60) NULL
+        )
+    ");
+
     $databaseConnection->query("DROP TABLE IF EXISTS articles;");
-    $databaseConnection->query("CREATE TABLE articles(id INTEGER PRIMARY KEY AUTO_INCREMENT, description TEXT NOT NULL)");
+
+    $databaseConnection->query("
+        CREATE TABLE articles(
+            id INTEGER PRIMARY KEY AUTO_INCREMENT,
+            description TEXT NOT NULL
+        )
+    ");
 
     echo "Datamodel migrated successfully." . PHP_EOL;
 } catch (Exception $exception) {
